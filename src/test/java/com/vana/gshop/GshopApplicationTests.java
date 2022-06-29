0cefc759f23ebec99ad083e30a7f733c71ca7b36
M.Said Gürbüz <42msg42@gmail.com>
2022-06-29 16:01:44 +0300

    first commit

---
+++ src/test/java/com/vana/gshop/GshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vana.gshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
